c8b4e7d033c05d6599e7b56577e52be84d07f074
fd-mailden <79415152+fd-mailden@users.noreply.github.com>
2021-05-14 12:37:41 +0300

    Add files via upload

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import { SliderData } from './SliderData';
+
+
+const ImageSlider = () => {
+    const[current, setCurrent] = useState(0)
+    const length = slides.length
+
+
+    return (
+        <>
+        {SliderData.map((slide, index)=>{
+            return <img src={slide.image} alt= " travel photo" />;
+            
+        })}
+        </>
+    );
+};
+
+export default ImageSlider
---
+++ src/components/SliderData.js
@@ -0,0 +1,12 @@
+export const SliderData = [
+
+    {
+        image: 'https://s3.nat-geo.ru/images/2019/5/16/27e419b1226a4b07ab4c60c95cd32049.max-2500x1500.jpg'
+    },
+    {
+        image: 'https://wallpaperaccess.com/full/7948.jpg'
+    },
+    {
+        image: 'https://i.pinimg.com/originals/98/c7/ce/98c7ce720941cff3bcc92e4b6c1da771.jpg'
+    }
+]
